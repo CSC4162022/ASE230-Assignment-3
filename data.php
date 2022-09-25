3401f24f092b9d17cc048db7b7256acf9f0a33fe
CD <98351932+CSC4162022@users.noreply.github.com>
2022-09-25 10:55:55 -0400

    Add files via upload

---
+++ data.php
@@ -0,0 +1,33 @@
+<?php
+
+//group information (fake information for other team members)
+const groupInfo = [
+    [
+        'name' => 'Cory DuCharme',
+        'designation' => 'Founder & CEO',
+        'gender' => 'male',
+        'year' => '&#9315;',
+        'picture'=> 'avatar7.png',
+        'birthdate' => "02-09-1989"
+    ],
+    [
+        'name' => 'Nazrul Islam',
+        'gender' => 'male',
+        'designation' => 'UI Designer',
+        'picture'=> 'avatar2.png',
+        'year' => '&#9313;',
+        'birthdate' => "05-23-1999"
+    ],
+    [
+        'name' => 'Riyadh Khan',
+        'gender' => 'male',
+        'designation' => 'Developer',
+        'picture'=> 'avatar6.png',
+        'year' => '&#9314;',
+        'birthdate' => "07-29-1999"
+    ]
+ ];
+
+
+
+
